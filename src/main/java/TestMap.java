80640fec8499751556bc7fbff631fb8616d12618
IrekNabiullin <08git08@mail.ru>
2021-02-07 14:37:12 +0300

    Changed Graphics g on myPalindromeString. Map examples added

--- src/main/java/TestMap.java
+++ src/main/java/TestMap.java
@@ -1,3 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class TestMap {
-    
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "string 1");
+        map.put(2, "string 2");
+        map.put(3, "string 3");
+        map.forEach((k, v) -> System.out.println(k + ": " + v));// вывод
+    }
 }
