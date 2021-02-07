aaeec056d2ae8a6c293e9b94022015344cb0b9b3
IrekNabiullin <08git08@mail.ru>
2021-02-07 14:42:47 +0300

    Changed Graphics g on myPalindromeString. Map examples added

--- src/main/java/TestMap.java
+++ src/main/java/TestMap.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class TestMap {
@@ -8,5 +9,23 @@ public class TestMap {
         map.put(2, "string 2");
         map.put(3, "string 3");
         map.forEach((k, v) -> System.out.println(k + ": " + v));// вывод
+//  Все пары (ключ-значение) хранятся во внутреннем интерфейсе Map.Entry, а чтобы получить их,
+//  нужно вызвать метод entrySet(). Он возвращает множество (Set) пар, которые можно перебрать в цикле:
+        for(Map.Entry<Integer, String> entry: map.entrySet()) {
+            // get key
+            Integer key = entry.getKey();
+            // get value
+            String value = entry.getValue();
+        }
+
+//        Или используя итератор:
+        Iterator<Map.Entry<Integer, String>> itr = map.entrySet().iterator();
+        while(itr.hasNext()) {
+            Map.Entry<Integer, String> entry =  itr.next();
+            // get key
+            Integer key = entry.getKey();
+            // get value
+            String value = entry.getValue();
+        }
     }
 }
