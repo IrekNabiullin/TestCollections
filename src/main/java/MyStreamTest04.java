c946c3f8355ef9eb446f4468955a8abd2b39a327
IrekNabiullin <08git08@mail.ru>
2021-04-04 20:28:04 +0300

    new examples added

---
+++ src/main/java/MyStreamTest04.java
@@ -0,0 +1,19 @@
+// Что будет выведено в консоли:
+
+import java.util.stream.Stream;
+
+public class MyStreamTest04 {
+
+    public static void main(String[] args) {
+        Stream<Integer> stream = Stream.of(1, 2, 3, 4, 5, 1, 2, 3);
+
+        stream
+                .distinct()
+                .peek(i -> System.out.println(i + " "))
+                .filter(element -> element >2)
+                .peek(i -> System.out.println(i + " "))
+                .map(element -> element * 10)
+                .peek(i -> System.out.println(i + " "))
+                .count();
+    }
+}
